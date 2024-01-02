71772bd754469eb9f20629b576168bfa3af0b979
AndrewWhalen <awhalen@gmail.com>
2024-01-02 10:10:36 -0800

    sadfasd

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ $file = 'people.txt';
 // Open the file to get existing content
 $temp = fopen($file, 'w') or die("Can't create file");
 
-$current = file_get_contents($temp);
+$current = file_get_contents($file);
 
 // Write the contents back to the file
 file_put_contents($file, $entityBody);
