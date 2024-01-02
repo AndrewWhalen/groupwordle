48dd5cd6f76bd99f3f2d2cb7bd72b07aa22a55f4
AndrewWhalen <awhalen@gmail.com>
2024-01-02 10:02:09 -0800

    asdfasd

--- test.php
+++ test.php
@@ -1,15 +1,15 @@
 <?php
-echo '<p>Hello World</p>';
+$entityBody = stream_get_contents(STDIN);
+
+echo $entityBody;
 $file = 'people.txt';
 
 // Open the file to get existing content
 $temp = fopen($file, 'w') or die("Can't create file");
 
-$current = file_get_contents($file);
-// Append a new person to the file
+$current = file_get_contents($temp);
 
-$current .= "John Smith\n";
 // Write the contents back to the file
-file_put_contents($file, $current);
+file_put_contents($file, $entityBody);
 
 ?>
