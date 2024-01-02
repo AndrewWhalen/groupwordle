532204687d5986bc33b80f0a9122ad89378c133f
AndrewWhalen <awhalen@gmail.com>
2024-01-02 09:32:24 -0800

    sadfasd

--- test.php
+++ test.php
@@ -1,10 +1,15 @@
 <?php
 echo '<p>Hello World</p>';
 $file = 'people.txt';
+
 // Open the file to get existing content
+$temp = fopen($file, 'w') or die("Can't create file");
+
 $current = file_get_contents($file);
 // Append a new person to the file
+
 $current .= "John Smith\n";
 // Write the contents back to the file
 file_put_contents($file, $current);
+
 ?>
