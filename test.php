acafa6b5eda1f6e03cf1fd668456561a2ee3494a
AndrewWhalen <awhalen@gmail.com>
2024-01-02 12:16:31 -0800

    add fix

--- test.php
+++ test.php
@@ -1,20 +1,13 @@
 <?php
 
 $entityBody = file_get_contents('php://input');
-
-if ($entityBody == "") {
-    echo "True";
-} else {
-    echo "False";
-}
 $file = 'people.txt';
 
-// Open the file to get existing content
-$temp = fopen($file, 'w') or die("Can't create file");
-
-$current = file_get_contents($file);
-
-// Write the contents back to the file
-file_put_contents($file, $entityBody);
+if ($entityBody != "") {
+    $temp = fopen($file, 'w') or die("Can't create file");
+    file_put_contents($file, $entityBody);
+} else {
+    echo $current = file_get_contents($file);
+}
 
 ?>
